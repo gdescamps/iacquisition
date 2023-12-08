10ea949a37b34b3b8167acb311f12eb1854b6428
Ubuntu <greg@plania-dev3.h25kzz5yd4kerik35ad3ffbwtd.ax.internal.cloudapp.net>
2023-12-08 19:23:12 +0000

    Creation doc api

---
+++ projet/api/JobDocument.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import json
+from PIL import Image
+import os
+import pytesseract
+from pdf2image import convert_from_path, convert_from_bytes
+
+class Document():
+    pass
+
+class JobDocument():
+    pass
+
+class CVDocument():
+    pass
