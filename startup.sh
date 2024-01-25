4109821feb1e6a63c5a361613f1210b0e70d5a3e
Ubuntu <pablo@iacquisition-dev.ohwudvv040jexn5bmps3jvvqjd.gvxx.internal.cloudapp.net>
2024-01-25 15:56:49 +0000

    Ajout requirements -- Migration azure

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 cd projet
 
 # Install the Python dependencies from the requirements.txt file
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Start Gunicorn with Uvicorn workers
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --preload
